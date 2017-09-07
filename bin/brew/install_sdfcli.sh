36d16511f150aa28fb74e2d1783990586ed23ddb
TJ Tyrrell <innovate@tjtyrrell.com>
2017-09-07 13:36:34 -0400

    master: Lets try this again, changed folder structure

--- bin/brew/install_sdfcli.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo "Installing SDFCLI"
-mkdir /usr/local/lib/sdfcli
-cp /usr/local/Cellar/sdfcli/17.2.0/com.netsuite.ide.core_2017.2.0.jar /usr/local/lib/sdfcli
-cp /usr/local/Cellar/sdfcli/17.2.0/pom.xml /usr/local/lib/sdfcli
-cp /usr/local/Cellar/sdfcli/17.2.0/sdfcli /usr/local/bin/sdfcli
-cp /usr/local/Cellar/sdfcli/17.2.0/sdfcli-createproject /usr/local/bin/sdfcli-createproject
