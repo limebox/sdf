2838e0cf3afdcbac91c9c813dc52a6fc604aa3b7
TJ Tyrrell <innovate@tjtyrrell.com>
2017-09-07 13:21:50 -0400

    master: Updated shell script to work

--- bin/brew/install_sdfcli.sh
+++ bin/brew/install_sdfcli.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-
+echo "Installing SDFCLI"
 mkdir /usr/local/lib/sdfcli
-cp /usr/local/Cellar/sdfcli/17.2.0/* /usr/local/lib/sdfcli
-mv /usr/local/lib/sdfcli/sdfcli /usr/local/bin
+cp /usr/local/Cellar/sdfcli/17.2.0/com.netsuite.ide.core_2017.2.0.jar /usr/local/lib/sdfcli
+cp /usr/local/Cellar/sdfcli/17.2.0/pom.xml /usr/local/lib/sdfcli
+cp /usr/local/Cellar/sdfcli/17.2.0/sdfcli /usr/local/bin/sdfcli
+cp /usr/local/Cellar/sdfcli/17.2.0/sdfcli-createproject /usr/local/bin/sdfcli-createproject
