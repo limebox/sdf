b394ebbab4c233ea3f550c28d2379657a01973e7
TJ Tyrrell <innovate@tjtyrrell.com>
2019-03-01 15:45:58 -0500

    19.1.0: With the latest update, I can remove a lot of extraneous files

--- Files/brew/sdfsdk.rb
+++ Files/brew/sdfsdk.rb
@@ -1,7 +1,7 @@
 class Sdfsdk < Formula
   desc "NetSuite SDF SDK Tool"
   homepage "https://limebox.com/developers/sdfsdk"
-  url "https://github.com/limebox/sdf/raw/18.2.1/Files/brew/sdfsdk-18.2.1.tar.gz"
+  url "https://system.netsuite.com/download/suitecloud-sdk/cli/java/19.1/sdf-cli.tar.gz"
   sha256 "7cc40481acb67147728b76e2c3330b27517cce90d8e03285d1aa76206d22893b"
 
   depends_on "maven" => :build
